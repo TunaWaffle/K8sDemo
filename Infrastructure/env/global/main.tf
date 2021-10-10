18c9b9cdf268a923ea855a9947adaaa174df99a0
Kevin Bost <kitokeboo@gmail.com>
2021-10-10 00:48:18 -0700

    Updating destroy
    Fixing CR name

--- Infrastructure/env/global/main.tf
+++ Infrastructure/env/global/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "global_rg" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "demo-container-registry"
+  name                = "democr"
   resource_group_name = azurerm_resource_group.global_rg.name
   location            = azurerm_resource_group.global_rg.location
   sku                 = "Basic"
