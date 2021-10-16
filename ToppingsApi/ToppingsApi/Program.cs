e8b591790c34fda08d8bd82be24495b821fa802b
Kevin Bost <kitokeboo@gmail.com>
2021-10-15 23:09:29 -0700

    Put back the https redirection

--- ToppingsApi/ToppingsApi/Program.cs
+++ ToppingsApi/ToppingsApi/Program.cs
@@ -28,6 +28,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ToppingsApi v1"));
 }
 
+app.UseHttpsRedirection();
+
 app.UseAuthentication();
 app.UseAuthorization();
 
