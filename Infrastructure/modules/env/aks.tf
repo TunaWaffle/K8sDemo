ca4f332544b8f0677b42ff3fa35382f2de241cb1
Kevin Bost <kitokeboo@gmail.com>
2021-10-17 20:05:43 -0700

    Reverting the naming of the MRG

--- Infrastructure/modules/env/aks.tf
+++ Infrastructure/modules/env/aks.tf
@@ -2,7 +2,6 @@ resource "azurerm_kubernetes_cluster" "app_cluster" {
   name                = "Demo-${var.environment}-aks"
   location            = azurerm_resource_group.app_rg.location
   resource_group_name = azurerm_resource_group.app_rg.name
-  node_resource_group = "Demo-${var.environment}-aks-mrg"
 
   api_server_authorized_ip_ranges = []
   dns_prefix                      = "tunawaffles${lower(var.environment)}"
@@ -26,8 +25,24 @@ resource "azurerm_kubernetes_cluster" "app_cluster" {
   }
 }
 
-resource "azurerm_role_assignment" "aks_acr" {
+data "azurerm_resource_group" "aks_managed_resource_group" {
+    name = azurerm_kubernetes_cluster.app_cluster.node_resource_group
+}
+
+resource "azurerm_role_assignment" "aks_acr_pull" {
   scope                = var.acr_id
   role_definition_name = "AcrPull"
   principal_id         = azurerm_kubernetes_cluster.app_cluster.kubelet_identity[0].object_id
+}
+
+resource "azurerm_role_assignment" "aks_monitoring_metrics_publisher" {
+  scope                = azurerm_kubernetes_cluster.app_cluster.id
+  role_definition_name = "Monitoring Metrics Publisher"
+  principal_id         = azurerm_kubernetes_cluster.app_cluster.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "aks_network_contributor" {
+  scope                = data.azurerm_resource_group.aks_managed_resource_group.id
+  role_definition_name = "Network Contributor"
+  principal_id         = azurerm_kubernetes_cluster.app_cluster.identity[0].principal_id
 }
