16d4391fff1b013cd05bd72cddd5853319f9f0f7
Kevin Bost <kitokeboo@gmail.com>
2021-10-21 22:37:01 -0700

    Starting work adding in EF Core
    Need to solve best way for devs to get access to SQL in dev environment

--- ToppingsApi/ToppingsApi.Tests/WebTestFixture.cs
+++ ToppingsApi/ToppingsApi.Tests/WebTestFixture.cs
@@ -1,6 +1,4 @@
 using Microsoft.AspNetCore.Mvc.Testing;
-using System;
-using Xunit;
 
 namespace ToppingsApi.Tests
 {
--- ToppingsApi/ToppingsApi/Startup.cs
+++ ToppingsApi/ToppingsApi/Startup.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Identity.Web;
 using Microsoft.OpenApi.Models;
+using ToppingsApi.Data;
 using ToppingsApi.HealthChecks;
 
 namespace ToppingsApi
@@ -30,6 +32,9 @@ namespace ToppingsApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ToppingsApi", Version = "v1" });
             });
+
+            services.AddDbContext<ToppingsContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("AppsDatabase")));
 
             services.AddHealthChecks()
                 .AddCheck<BasicHealthCheck>("Basic");
