71afde594ff029e3fbac20769bc895cdb18f98cd
Kevin Bost <kitokeboo@gmail.com>
2021-10-25 16:58:07 -0700

    Script work

--- Setup.ps1
+++ Setup.ps1
@@ -4,6 +4,16 @@
 $resourceGroup = "Demo-Dev"
 $sqlServer = "demodevsql"
 $databaseName = "app-db"
+$storageContainer = "sqlbackups"
+$bacpacFile = "dev.bacpac"
+
+$sqlBackupStorageAccount = $(az storage account list -g "$resourceGroup" --query "[? starts_with(name, 'sqlbackup')].name | [0]")
+
+az storage blob generate-sas --account-name $sqlBackupStorageAccount -c $storageContainer -n "$bacpacFile" --permissions w --expiry 2018-01-01T00:00:00Z
+$accessKey = $(az storage account keys list -n $storageAccount -g "$resourceGroup" --query [0].value)
+az sql db export -s $sqlServer -n $databaseName -g "$resourceGroup" -p password -u login \
+--storage-key $accessKey --storage-key-type StorageAccessKey \
+--storage-uri https://myAccountName.blob.core.windows.net/myContainer/myBacpac.bacpac
 
 $ip = (Invoke-WebRequest http://ident.me).Content;
 $currentUser = $(az account show --query user.name);
