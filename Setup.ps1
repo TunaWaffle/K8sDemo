7e4d2b72bb59b21a0bf8a57d1e132fc0cb11d55c
Kevin Bost <kitokeboo@gmail.com>
2021-10-25 21:44:49 -0700

    Adding script to create LocalDB instance from Azure SQL

--- Setup.ps1
+++ Setup.ps1
@@ -1,28 +1,78 @@
 # This script is expected to be run after the DEV SQL server is deployed
 # You must be logged into your azure account with access to the SQL server
 
+# It will load a copy of the dev database into a LocalDb instance. If you
+# don't have LocalDb installed it can be done from the Visual Studio installer:
+# More information: https://docs.microsoft.com/sql/database-engine/configure-windows/sql-server-express-localdb 
+
+
+
 $resourceGroup = "Demo-Dev"
 $sqlServer = "demodevsql"
 $databaseName = "app-db"
 $storageContainer = "sqlbackups"
 $bacpacFile = "dev.bacpac"
 
-$sqlBackupStorageAccount = $(az storage account list -g "$resourceGroup" --query "[? starts_with(name, 'sqlbackup')].name | [0]")
-$accessKey = $(az storage account keys list -n $storageAccount -g "$resourceGroup" --query [0].value)
+$user = az account show --query user.name
+$blobName = "$user-$bacpacFile"
 
-pushd Infrastructure/env/dev
+Write-Host "Creating database backup for $user"
+
+Write-Host "Getting storage account for SQL backup"
+$sqlBackupStorageAccount = $(az storage account list -g "$resourceGroup" --query "[? starts_with(name, 'sqlbackup')].name | [0]")
+$accessKey = $(az storage account keys list -n $sqlBackupStorageAccount -g "$resourceGroup" --query [0].value)
+
+Write-Host "Getting SQL database credentials"
+Push-Location Infrastructure/env/dev
 $adminLogin = $(terraform output sql_server_admin_login)
 $adminPassword = $(terraform output sql_server_admin_password)
-popd
+Pop-Location
 
-az sql db export -s $sqlServer -n $databaseName -g "$resourceGroup" -p $adminPassword -u $adminLogin `
---storage-key $accessKey --storage-key-type StorageAccessKey `
---storage-uri "https://$sqlBackupStorageAccount.blob.core.windows.net/$storageContainer/$bacpacFile"
+Write-Host "Exporting database backup"
+az sql db export `
+    --server "$sqlServer" `
+    --name "$databaseName" `
+    --resource-group "$resourceGroup" `
+    --admin-user "$adminLogin" `
+    --admin-password "$adminPassword" `
+    --storage-key $accessKey `
+    --storage-key-type StorageAccessKey `
+    --storage-uri "https://$sqlBackupStorageAccount.blob.core.windows.net/$storageContainer/$blobName" `
+    | Out-Null
 
-#$ip = (Invoke-WebRequest http://ident.me).Content;
-#$currentUser = $(az account show --query user.name);
-#az sql server firewall-rule create --name "DEV-$currentUser" --resource-group "$resourceGroup" --server "$sqlServer" --start-ip-address "$ip" --end-ip-address "$ip"
-#$connectionString = $(az sql db show-connection-string --name "$databaseName" --server "$sqlServer" --client ado.net --auth-type ADIntegrated)
-##trim off quotes
-#$connectionString = $connectionString.Substring(1, $connectionString.Length - 2)
-#dotnet user-secrets set "ConnectionStrings:AppsDatabase" "$connectionString" --project .\ToppingsApi\ToppingsApi\ToppingsApi.csproj
+Write-Host "Downloading database backup from blob"
+az storage blob download `
+    --container-name "$storageContainer" `
+    --file "$bacpacFile" `
+    --name "$blobName" `
+    --account-name "$sqlBackupStorageAccount" `
+    --account-key "$accessKey" `
+    | Out-Null
+
+Write-Host "Removing blob"
+az storage blob delete `
+    --container-name "$storageContainer" `
+    --name "$blobName" `
+    --account-name "$sqlBackupStorageAccount" `
+    --account-key "$accessKey" `
+    | Out-Null
+
+Write-Host "Restoring to LocalDb"
+$vsWherePath = "${env:ProgramFiles(x86)}/Microsoft Visual Studio/Installer/vswhere.exe"
+if (!(Test-Path $vsWherePath)) {
+    Write-Error "Could not find vswhere.exe at $vsWherePath. Ensure Visual Studio is installed."
+    return 1
+}
+$vsInstallLocation = & $vsWherePath -all -latest -prerelease -format value -property installationPath
+$sqlPackageLocation  = "$vsInstallLocation\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\SqlPackage.exe"
+if (!(Test-Path $sqlPackageLocation)) {
+    Write-Error "Could not find SqlPackage.exe at $sqlPackageLocation. Ensure LocalDb is installed via the Visual Studio Installer."
+    return 1
+}
+
+$connectionString = "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=$databaseName;Integrated Security=true;"
+& $sqlPackageLocation /Action:Import /SourceFile:"$bacpacFile" /TargetConnectionString:"$connectionString"
+Remove-Item $bacpacFile
+
+Write-Host "Updating ToppingsApi secrets"
+dotnet user-secrets set "ConnectionStrings:AppsDatabase" "$connectionString" --project .\ToppingsApi\ToppingsApi\ToppingsApi.csproj
