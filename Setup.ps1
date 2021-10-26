7f97f97907be0107fd7253df4753370966b17664
Kevin Bost <kitokeboo@gmail.com>
2021-10-25 17:13:39 -0700

    Looking up creds from state file.

--- Setup.ps1
+++ Setup.ps1
@@ -8,17 +8,21 @@ $storageContainer = "sqlbackups"
 $bacpacFile = "dev.bacpac"
 
 $sqlBackupStorageAccount = $(az storage account list -g "$resourceGroup" --query "[? starts_with(name, 'sqlbackup')].name | [0]")
-
-az storage blob generate-sas --account-name $sqlBackupStorageAccount -c $storageContainer -n "$bacpacFile" --permissions w --expiry 2018-01-01T00:00:00Z
 $accessKey = $(az storage account keys list -n $storageAccount -g "$resourceGroup" --query [0].value)
-az sql db export -s $sqlServer -n $databaseName -g "$resourceGroup" -p password -u login \
---storage-key $accessKey --storage-key-type StorageAccessKey \
---storage-uri https://myAccountName.blob.core.windows.net/myContainer/myBacpac.bacpac
 
-$ip = (Invoke-WebRequest http://ident.me).Content;
-$currentUser = $(az account show --query user.name);
-az sql server firewall-rule create --name "DEV-$currentUser" --resource-group "$resourceGroup" --server "$sqlServer" --start-ip-address "$ip" --end-ip-address "$ip"
-$connectionString = $(az sql db show-connection-string --name "$databaseName" --server "$sqlServer" --client ado.net --auth-type ADIntegrated)
-#trim off quotes
-$connectionString = $connectionString.Substring(1, $connectionString.Length - 2)
-dotnet user-secrets set "ConnectionStrings:AppsDatabase" "$connectionString" --project .\ToppingsApi\ToppingsApi\ToppingsApi.csproj
+pushd Infrastructure/env/dev
+$adminLogin = $(terraform output sql_server_admin_login)
+$adminPassword = $(terraform output sql_server_admin_password)
+popd
+
+az sql db export -s $sqlServer -n $databaseName -g "$resourceGroup" -p $adminPassword -u $adminLogin `
+--storage-key $accessKey --storage-key-type StorageAccessKey `
+--storage-uri "https://$sqlBackupStorageAccount.blob.core.windows.net/$storageContainer/$bacpacFile"
+
+#$ip = (Invoke-WebRequest http://ident.me).Content;
+#$currentUser = $(az account show --query user.name);
+#az sql server firewall-rule create --name "DEV-$currentUser" --resource-group "$resourceGroup" --server "$sqlServer" --start-ip-address "$ip" --end-ip-address "$ip"
+#$connectionString = $(az sql db show-connection-string --name "$databaseName" --server "$sqlServer" --client ado.net --auth-type ADIntegrated)
+##trim off quotes
+#$connectionString = $connectionString.Substring(1, $connectionString.Length - 2)
+#dotnet user-secrets set "ConnectionStrings:AppsDatabase" "$connectionString" --project .\ToppingsApi\ToppingsApi\ToppingsApi.csproj
