a946a5d4119149517c637414cda2c5904af1583b
Kevin B <Keboo@users.noreply.github.com>
2021-10-25 17:01:28 -0700

    Marking as sensitive

--- Infrastructure/env/dev/outputs.tf
+++ Infrastructure/env/dev/outputs.tf
@@ -1,7 +1,9 @@
 output "sql_server_admin_password" {
-    value = module.env.sql_server.administrator_login_password
+    value     = module.env.sql_server.administrator_login_password
+    sensitive = true
 }
 
 output "sql_server_admin_login" {
-    value = module.env.sql_server.administrator_login
+    value     = module.env.sql_server.administrator_login
+    sensitive = true
 }
